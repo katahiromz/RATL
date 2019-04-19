f8e37872b3c7bc829f6f8190cf5e4c41f2d88dc5
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2019-04-19 20:47:07 +0900

    update sync batch files

--- from-reactos.sh
+++ from-reactos.sh
@@ -4,4 +4,5 @@ cp -r ../reactos/modules/rostests/apitests/atl/*.bmp atltests
 cp -r ../reactos/modules/rostests/apitests/atl/*.cpp atltests
 cp -r ../reactos/modules/rostests/apitests/atl/*.inl atltests
 cp -r ../reactos/modules/rostests/apitests/atl/*.h atltests
-cp -r ../reactos/modules/rostests/apitests/atl/devenv atltests
+cp -r ../reactos/modules/rostests/apitests/atl/devenv/*.sln atltests/devenv
+cp -r ../reactos/modules/rostests/apitests/atl/devenv/*.vcxproj atltests/devenv
