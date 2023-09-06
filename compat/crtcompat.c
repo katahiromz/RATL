ec8a4898bd8d2d6089f4747e5ddfbdf4523cf2b8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-09-06 15:13:50 +0900

    improve compat/crtcompat.c

--- compat/crtcompat.c
+++ compat/crtcompat.c
@@ -9,3 +9,13 @@ int __cdecl _imp___CrtDbgReportW(int reportType, const wchar_t *filename, int li
 {
     return 0;
 }
+
+int __cdecl __imp___CrtDbgReport(int reportType, const char *filename, int linenumber, const char *moduleName, const char *format, ...)
+{
+    return 0;
+}
+
+int __cdecl __imp___CrtDbgReportW(int reportType, const wchar_t *filename, int linenumber, const wchar_t *moduleName, const wchar_t *format, ...)
+{
+    return 0;
+}
