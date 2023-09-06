be977548be391723ebd2aaadababc82747def062
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-09-06 13:10:33 +0900

    fix

--- compat/crtcompat.c
+++ compat/crtcompat.c
@@ -1,11 +1,11 @@
 #include <windows.h>
 
-int __cdecl __imp__CrtDbgReport(int reportType, const char *filename, int linenumber, const char *moduleName, const char *format, ...)
+int __cdecl _imp___CrtDbgReport(int reportType, const char *filename, int linenumber, const char *moduleName, const char *format, ...)
 {
     return 0;
 }
 
-int __cdecl __imp__CrtDbgReportW(int reportType, const wchar_t *filename, int linenumber, const wchar_t *moduleName, const wchar_t *format, ...)
+int __cdecl _imp___CrtDbgReportW(int reportType, const wchar_t *filename, int linenumber, const wchar_t *moduleName, const wchar_t *format, ...)
 {
     return 0;
 }
